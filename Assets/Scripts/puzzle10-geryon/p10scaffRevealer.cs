1fdc500d86f49176cc27a447f8ef5616828f76cd
Unknown <mette.tanggaard.ohlsen@gmail.com>
2020-04-27 13:29:20 +0200

    puzzle 10 animation error

--- Assets/Scripts/puzzle10-geryon/p10scaffRevealer.cs
+++ Assets/Scripts/puzzle10-geryon/p10scaffRevealer.cs
@@ -5,8 +5,6 @@ using Mirror;
 
 public class p10scaffRevealer : NetworkReveiler
 {
-    public GameObject chestLid;
-    public GameObject keyItem;
     public GameObject fence;
     public bool hasReveiled = false;
 
@@ -15,7 +13,6 @@ public class p10scaffRevealer : NetworkReveiler
         if (!isServer) return;
         if (!hasReveiled) {
             RpcOpenChest();
-            keyItem.GetComponent<ShowObjects>().moveToPosition();
             hasReveiled = true;
         }
 
