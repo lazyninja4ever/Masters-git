1542287386d88072a601a383503d85cb68f9aa93
Unknown <mette.tanggaard.ohlsen@gmail.com>
2020-04-27 11:30:54 +0200

    puzzle 10

---
+++ Assets/Scripts/puzzle10-geryon/p10scaffRevealer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class p10scaffRevealer : NetworkReveiler
+{
+    public GameObject chestLid;
+    public GameObject keyItem;
+    public GameObject fence;
+    public bool hasReveiled = false;
+
+    public override void ReveilPrice() {
+        base.ReveilPrice();
+        if (!isServer) return;
+        if (!hasReveiled) {
+            RpcOpenChest();
+            keyItem.GetComponent<ShowObjects>().moveToPosition();
+            hasReveiled = true;
+        }
+
+
+    }
+
+    [ClientRpc]
+    void RpcOpenChest() {
+        fence.GetComponent<showWithAnim>().ShowObject();
+    }
+}
