59a645fec56077b45bb5af3ecb00c58a65045f8d
Natascha <natgroenlie@outlook.dk>
2020-04-27 15:49:38 +0200

    puzzle 6

--- Assets/Scripts/Puzzle6-Birds/ScaffoldingRotate.cs
+++ Assets/Scripts/Puzzle6-Birds/ScaffoldingRotate.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class RotateBricks : NetworkInteraction
+public class ScaffoldingRotate : NetworkInteraction
 {
     public bool isRotating;
     public int currentAngle = 0;
-    public SolutionMusic solutionScript;
-    public AudioSource brickSound;
-    
+    public ScaffoldingSolution scaffSolutionScript;
+  //  public AudioSource brickSound;
+
     public override void InterActionFuntion(GameObject player)
     {
         if (!isServer) return;
         base.InterActionFuntion(player);
-        if(isRotating == false)
+        if (isRotating == false)
         {
             RpcRunRotate(90);
-            solutionScript.Invoke("RpcCheckboard", 1f);
-        }   
+            scaffSolutionScript.Invoke("RpcCheckboardScaf", 1f);
+        }
     }
 
-    public IEnumerator Rotate( Vector3 axis, float angle, float duration = 1.0f)
+    public IEnumerator Rotate(Vector3 axis, float angle, float duration = 1.0f)
     {
         isRotating = true;
-        brickSound.Play();
+    //    brickSound.Play();
         Quaternion from = transform.rotation;
         Quaternion to = transform.rotation;
         to *= Quaternion.Euler(axis.x, axis.y, angle);
 
         float elapsed = 0.0f;
-        while(elapsed < duration)
+        while (elapsed < duration)
         {
             transform.rotation = Quaternion.Slerp(from, to, elapsed / duration);
             elapsed += Time.deltaTime;
@@ -40,14 +40,15 @@ public class RotateBricks : NetworkInteraction
         currentAngle++;
         isRotating = false;
 
-        if(currentAngle == 4)
+        if (currentAngle == 4)
         {
             currentAngle = 0;
         }
     }
 
     [ClientRpc]
-    public void RpcRunRotate(int degrees) {
+    public void RpcRunRotate(int degrees)
+    {
         StartCoroutine(Rotate(Vector3.left, degrees, 0.2f));
     }
 }
---
+++ Assets/Scripts/Puzzle6-Birds/ScaffoldingSolution.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ScaffoldingSolution : NetworkBehaviour
+{
+    public List<int> CorrectRotation = new List<int>();
+    public List<int> currentRotation = new List<int>();
+    public GameObject[] bricks;
+ //   public AudioSource rotationSolvedSound;
+
+    public void CallDeactivate()
+    {
+        if (!isServer) return;
+        RpcDeactivate();
+    }
+
+    public bool RotationCheck()
+    {
+        for (int i = 0; i < currentRotation.Count; i++)
+        {
+            if (currentRotation[i] != CorrectRotation[i])
+            {
+                Debug.Log("is false");
+                return false;
+            }
+        }
+        return true; 
+    }
+
+    [ClientRpc]
+    public void RpcCheckboardScaf()
+    {
+        for (int i = 0; i < bricks.Length; i++)
+        {
+            currentRotation.Add(bricks[i].GetComponent<RotateBricks>().currentAngle);
+        }
+        
+        if (RotationCheck())
+        {
+            CallDeactivate();
+        }
+        else
+        {
+         //   currentRotation.Clear();
+        }
+    }
+
+    [ClientRpc]
+    public void RpcDeactivate()
+    {
+        for (int i = 0; i < bricks.Length; i++)
+        {
+            bricks[i].GetComponent<RotateBricks>().InteractMsg = "";
+            bricks[i].GetComponent<RotateBricks>().isRotating = true;
+        }
+  //      rotationSolvedSound.Play();
+    }
+}
